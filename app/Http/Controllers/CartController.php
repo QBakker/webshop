6081982732ae21f7485fac038966e428aa8326d4
Quinten <quinten_bakker@live.nl>
2020-01-17 18:16:45 +0100

    + Cart

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cart;
+use App\Product;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $items = $request->session()->get('cart_items');
+
+        if (!$items) {
+            $items = [];
+        }
+        
+        // & teken houdt de koppeling tussen de $items array en de index.
+        // In dit geval is $item gekoppeld aan $items[0], $items[1], $items[2];
+        $totaal = 0;
+        foreach ($items as &$item) {
+            $item['product'] = Product::find($item['id']);
+
+            $totaal = $totaal + ($item['product']->price * $item['amount']);
+
+            // $item wordt vergeten.
+            unset($item);
+        }
+
+        // Hier niet meer toevallig de laatste $item aanpast.
+        
+        return view('cart.index', ['items' => $items, 'totaal' => $totaal]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $this->validate($request, [
+            'productID' => 'required|exists:products,id',
+            'amount' => 'required',
+        ]);
+
+        $items = $request->session()->get('cart_items');
+
+        if (!$items) {
+            $items = [];
+        }
+        
+        $found = false;
+        foreach ($items as $item) {
+            if ($item['id'] === $validated['productID']) {
+                $found = true;
+            }
+        }
+
+        if (!$found) {
+            $items[] = [
+                'id' => $validated['productID'],
+                'amount' => $validated['amount']
+            ];
+        } else {
+            foreach ($items as &$item) {
+                if ($item['id'] === $validated['productID']) {
+
+                    if ($validated['amount'] == 0) {
+                        return $this->destroy($request);
+                    }
+
+                    $item['amount'] = $validated['amount'];
+                }
+                unset($item);
+            }
+
+        }
+
+        $request->session()->put('cart_items', $items);
+        
+        return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $validated = $this->validate($request, [
+            'productID' => 'required|exists:products,id',
+        ]);
+
+        $items = $request->session()->get('cart_items');
+
+        if (!$items) {
+            $items = [];
+        }
+        
+        $found = false;
+        foreach ($items as $index => $item) {
+            if ($item['id'] === $validated['productID']) {
+                $found = $index;
+            }
+        }
+
+        if ($found !== false) {
+            array_splice($items, $index, 1);
+        }
+
+        $request->session()->put('cart_items', $items);   
+        
+        return back();
+    }
+}
---
+++ resources/views/cart/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 my-5">
+            <h1 class="text-center text-white">Winkelwagen:</h1>
+        </div>
+    </div>
+    <div class="col-12">
+        <table class="table table-striped table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">Artikel</th>
+                    <th scope="col">Naam</th>
+                    <th scope="col">Beschrijving</th>
+                    <th scope="col">Hoeveelheid</th>
+                    <th scope="col">Bedrag</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @if( $items === [] )
+                <tr>
+                    <td colspan='5'>De winkelwagen is leeg</td>
+                </tr>
+                @endif
+                @foreach ($items as $item)
+                <tr class="custom-table">
+                    <td class="table-img">
+                        <img src="/img/product/{{ $item['product']->image }}" alt="{{ $item['product']->name }}">
+                    </td>
+                    <td class="w-25">{{ $item['product']->name }}</td>
+                    <td class="w-25">{{ $item['product']->description }}</td>
+                    <td class="w-25">
+                        <form action="{{ route('cart.store') }}" method="post">
+                            @csrf
+                            <input type="hidden" name="productID" value="{{ $item['product']->id }}">
+                            <input class="d-block mb-2" type="number" name="amount" value="{{ $item['amount'] }}" min="0">
+                            <button type="submit">Pas aan</button>
+                        </form>
+                    </td>
+                    <td>{{ number_format($item['product']->price, 2, ',', '.') }}</td>
+                    <td>
+                        <form action="{{ route('cart.destroy') }}" method="post">
+                            @method('DELETE')
+                            @csrf
+                            <input type="hidden" name="productID" value="{{ $item['product']->id }}">
+                            <button type="submit">X</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        @if ( $items != [] )
+        <div class="row">
+            <div class="col-10 text-white">
+                <h6>Totaal: &euro; {{ number_format($totaal, 2, ',', '.') }}</h6>
+            </div>
+            <div class="col-2">
+                <a href="{{ route('order.create') }}" class="btn btn-success text-decoration-none">Bestellen</a>
+            </div>
+        </div>
+        @endif
+    </div>
+</div>
+@endsection
